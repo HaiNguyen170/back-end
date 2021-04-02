bd3e60fa28b48757ff8f822a8aee46987c035c61
QuangMinh3110 <55874218+QuangMinh3110@users.noreply.github.com>
2021-04-03 02:57:03 +0700

    aa

--- src/main/java/com/example/demo/MegazineProjectApplication.java
+++ src/main/java/com/example/demo/MegazineProjectApplication.java
@@ -1,13 +1,13 @@
-package com.example.emailapp;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MegazineSpringMasterApplication {
+public class MegazineProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MegazineSpringMasterApplication.class, args);
+		SpringApplication.run(MegazineProjectApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/MegazineProjectApplicationTests.java
+++ src/test/java/com/example/demo/MegazineProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.emailapp;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MegazineSpringMasterApplicationTests {
+class MegazineProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
