a25587e01a968167c4818b9d210858ed2645792e
QuangMinh3110 <55874218+QuangMinh3110@users.noreply.github.com>
2021-04-03 02:43:31 +0700

    springboot

---
+++ src/main/java/com/example/emailapp/MegazineSpringMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegazineSpringMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegazineSpringMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emailapp/MegazineSpringMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegazineSpringMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
