cf4b3519568c701c0955258a323b000a7226f09f
QuangMinh3110 <55874218+QuangMinh3110@users.noreply.github.com>
2021-04-04 23:10:45 +0700

    1

--- src/main/java/com/example/demo/models/EFalcuty.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.models;
-
-public enum EFalcuty {
-	FALCUTY_SE,
-	FALCUTY_IB,
-	FALCUTY_SA,
-	FALCUTY_AI
-}
--- src/main/java/com/example/demo/models/ERole.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.models;
-
-public enum ERole {
-	ROLE_MKT_COORDINATOR,
-	ROLE_MKT_MANAGER,
-	ROLE_ADMIN,
-	ROLE_STUDENT
-}
